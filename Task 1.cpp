f3fa8e4b7a2e8674507d4c25ec4827e48a02212c
ned66 <ned66@DENIS>
2025-02-23 12:51:34 +0500

    Task 2

--- Task 1.cpp
+++ Task 1.cpp
@@ -1,10 +1,40 @@
-﻿// LessonNumber26.TextProcessing.cpp 
+﻿// Написать функцию, которая удаляет из строки
+// символ с заданным номером
 
 #include <iostream>
 
-int main()
+char DelSymbol(char str[], int symbol)
 {
-    std::cout << "Hello World!\n";
+    for (int i = 1; i < strlen(str); i++)
+    {
+        if (i == symbol)
+        {
+            str[i] = str[i + 1];
+        }
+        else
+            str[i] = str[i];
+    }
+    return *str;
 }
 
+int main()
+{
+    setlocale(LC_ALL, "");
+    std::cout << "Добро пожаловать в программу удаления из строки символа с заданным номером!\n";
+    char string[] = "Hello world!";
+    for (int i = 0; i < strlen(string); i++)
+    {
+        std::cout << "\n[" << i << "] \t" << string[i] << std::endl;
+    }
 
+    std::cout << "Укажите символ который требуется удалить: "; 
+    int number;
+    std::cin >> number;
+
+    DelSymbol(string, number);
+
+    for (int i = 0; i < strlen(string); i++)
+    {
+        std::cout << "\n[" << i << "] \t" << string[i] << std::endl;
+    }  
+}
---
+++ Task 2.cpp
@@ -0,0 +1,40 @@
+// Написать функцию, которая удаляет из строки
+// все вхождения в нее заданного символа.
+
+#include <iostream>
+
+
+void DelSymbol(char *str, char symbol)
+{
+    int j = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] != symbol)
+        {
+            str[j++] = str[i];
+        }
+    }
+    str[j] = '\0';
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    std::cout << "Добро пожаловать в программу удаления из строки символа с заданным номером!\n";
+    char string[] = "Hello world!";
+    for (int i = 0; i < strlen(string); i++)
+    {
+        std::cout << "\n[" << i << "] \t" << string[i] << std::endl;
+    }
+
+    std::cout << "Укажите номер символа который требуется удалить: ";
+    char str;
+    std::cin >> str;
+
+    DelSymbol(string, str);
+
+    for (int i = 0; i < strlen(string); i++)
+    {
+        std::cout << "\n[" << i << "] \t" << string[i] << std::endl;
+    }
+}
